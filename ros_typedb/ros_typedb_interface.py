3f607a139485a65b39aea53a544baf445d73bf60
Gustavo <g.rezendesilva@tudelft.nl>
2023-06-15 13:53:32 +0200

    ✨ make it a ROS 2 package

---
+++ ros_typedb/ros_typedb_interface.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ros_typedb.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'ros_typedb'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Gustavo Rezende',
+    maintainer_email='g.rezendesilva@tudelft.nl',
+    description='ROS interface for TypeDB',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ros_typedb_interface = ros_typedb.ros_typedb_interface:main'
+        ],
+    },
+)
