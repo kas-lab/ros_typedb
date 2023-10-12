29ef0b86147bcabc27d545bd535cce4100b4e78c
Gustavo <g.rezendesilva@tudelft.nl>
2023-10-12 16:41:13 +0200

    👷 update typedb install script

--- .github/workflows/install_typedb.sh
+++ .github/workflows/install_typedb.sh
@@ -3,8 +3,9 @@
 sudo apt install software-properties-common apt-transport-https gpg
 gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 8F3DA4B5E9AEF44C
 gpg --export 8F3DA4B5E9AEF44C | sudo tee /etc/apt/trusted.gpg.d/vaticle.gpg > /dev/null
-echo "deb [ arch=all ] https://repo.vaticle.com/repository/apt/ trusty main" | sudo tee /etc/apt/sources.list.d/vaticle.list > /dev/null
+echo "deb https://repo.vaticle.com/repository/apt/ trusty main" | sudo tee /etc/apt/sources.list.d/vaticle.list > /dev/null
 
 sudo apt update
-sudo apt install typedb-all
-pip3 install typedb-client
+sudo apt install openjdk-11-jre
+sudo apt install typedb-server=2.24.17 typedb-console=2.24.15 typedb-bin=2.24.16
+pip3 install typedb-driver
