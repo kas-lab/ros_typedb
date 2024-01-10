7d81d93a48653a04aea9fac3346869d6f0d529e4
Gustavo <g.rezendesilva@tudelft.nl>
2024-01-10 14:23:46 -0300

    📝 add docstring & typing to ros_typedb_interface

--- ros_typedb/ros_typedb/ros_typedb_interface.py
+++ ros_typedb/ros_typedb/ros_typedb_interface.py
@@ -13,6 +13,9 @@
 # limitations under the License.
 """ros_typedb_interface - python interface to interact with typedb via ROS."""
 
+import rcl_interfaces
+import ros_typedb_msgs
+
 from rcl_interfaces.msg import ParameterValue
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.callback_groups import ReentrantCallbackGroup
@@ -20,14 +23,28 @@ from rclpy.lifecycle import Node
 from rclpy.lifecycle import State
 from rclpy.lifecycle import TransitionCallbackReturn
 
+from ros_typedb.typedb_interface import MatchResultDict
 from ros_typedb.typedb_interface import TypeDBInterface
 from ros_typedb_msgs.msg import Attribute
 from ros_typedb_msgs.srv import Query
 
 from std_msgs.msg import String
 
+from typing import Literal
+from typing import Optional
 
-def set_query_result_value(value, value_type):
+
+def set_query_result_value(
+     value: bool | int | float | str | list[bool] | list[int] | list[float]
+     | list[str],
+     value_type: str) -> rcl_interfaces.msg.ParameterValue:
+    """
+    Convert value to :class:`rcl_interfaces.msg.ParameterValue`.
+
+    :param value: value to be converted
+    :param value_type: value type, e.g., `boolean`, `float` etc.
+    :return: converted value
+    """
     _param_value = ParameterValue()
     _type_dict = {
         'boolean': (1, 'bool_value'),
@@ -54,7 +71,15 @@ def set_query_result_value(value, value_type):
     return _param_value
 
 
-def match_query_result_to_ros_msg(query_result):
+def match_query_result_to_ros_msg(
+     query_result: list[dict[str, MatchResultDict]] | None
+     ) -> ros_typedb_msgs.srv.Query.Response:
+    """
+    Convert typedb match query result to :class:`ros_typedb_msgs.srv.Query`.
+
+    :param query_result: typedb match query result.
+    :return: converted query response.
+    """
     response = Query.Response()
     for result in query_result:
         for key, value_dict in result.items():
@@ -70,7 +95,14 @@ def match_query_result_to_ros_msg(query_result):
     return response
 
 
-def match_aggregate_query_result_to_ros_msg(query_result):
+def match_aggregate_query_result_to_ros_msg(
+     query_result: int | float | None) -> ros_typedb_msgs.srv.Query.Response:
+    """
+    Convert match aggregate query result to :class:`ros_typedb_msgs.srv.Query`.
+
+    :param query_result: typedb match aggreate query result.
+    :return: converted query response.
+    """
     response = Query.Response()
     _attr = Attribute()
     _attr.value = set_query_result_value(
@@ -80,7 +112,17 @@ def match_aggregate_query_result_to_ros_msg(query_result):
     return response
 
 
-def query_result_to_ros_msg(query_type, query_result):
+def query_result_to_ros_msg(
+     query_type: Literal['match', 'match_aggregate'],
+     query_result: list[dict[str, MatchResultDict]] | int | float | None
+     ) -> ros_typedb_msgs.srv.Query.Response:
+    """
+    Convert typedb query result to :class:`ros_typedb_msgs.srv.Query`.
+
+    :param query_type: query_type, e.g., 'match' or 'match_aggregate'
+    :param query_result: typedb  query result.
+    :return: converted query response.
+    """
     response = Query.Response()
     if query_type == 'match':
         response = match_query_result_to_ros_msg(query_result)
@@ -90,9 +132,10 @@ def query_result_to_ros_msg(query_type, query_result):
 
 
 class ROSTypeDBInterface(Node):
-    """ROS node to interact with typedb."""
+    """ROS lifecycle node to interact with typedb."""
 
-    def __init__(self, node_name, **kwargs):
+    def __init__(self, node_name: str, **kwargs):
+        """Create ROSTypeDBInterface node, inherits from lifecycle node."""
         super().__init__(node_name, **kwargs)
         self.declare_parameter('address', 'localhost:1729')
         self.declare_parameter('database_name', 'ros_typedb')
@@ -110,14 +153,24 @@ class ROSTypeDBInterface(Node):
 
     def init_typedb_interface(
             self,
-            address,
-            database_name,
-            schema_path=None,
-            data_path=None,
-            force_database=False,
-            force_data=False,
-            infer=False):
+            address: str,
+            database_name: str,
+            schema_path: Optional[list[str] | str] = None,
+            data_path: Optional[list[str] | str] = None,
+            force_database: Optional[bool] = False,
+            force_data: Optional[bool] = False,
+            infer: Optional[bool] = False) -> None:
+        """
+        Initialize self.typedb_interface.
 
+        :param address: TypeDB server address.
+        :param database_name: database name.
+        :param schema_path: list with paths to schema files (.tql).
+        :param data_path: list with paths to data files (.tql).
+        :param force_database: if database should override an existing database
+        :param force_data: if the database data should be overriden.
+        :param infer: if inference engine should be used.
+        """
         self.typedb_interface = self.typedb_interface_class(
             address,
             database_name,
@@ -131,16 +184,28 @@ class ROSTypeDBInterface(Node):
         self.typedb_interface.insert_data_event = self.insert_data_event
         self.typedb_interface.delete_data_event = self.delete_data_event
 
-    def publish_data_event(self, event_type):
+    def publish_data_event(self, event_type: str) -> None:
+        """
+        Publish message in the `/event` topic.
+
+        :param event_type: event to be published, e.g., 'insert' or 'delete'.
+        """
         self.event_pub.publish(String(data=event_type))
 
-    def insert_data_event(self):
+    def insert_data_event(self) -> None:
+        """Publish 'insert' in the /event topic"""
         self.publish_data_event('insert')
 
-    def delete_data_event(self):
+    def delete_data_event(self) -> None:
+        """Publish 'delete' in the /event topic"""
         self.publish_data_event('delete')
 
     def on_configure(self, state: State) -> TransitionCallbackReturn:
+        """
+        Configure ROSTypeDBInterface when the configure transition is called.
+
+        :return: transition result
+        """
         self.get_logger().info(self.get_name() + ': on_configure() is called.')
 
         self.init_typedb_interface(
@@ -169,15 +234,31 @@ class ROSTypeDBInterface(Node):
         return TransitionCallbackReturn.SUCCESS
 
     def on_cleanup(self, state: State) -> TransitionCallbackReturn:
+        """
+        Cleanup ROSTypeDBInterface when the cleanup transition is called.
+
+        :return: transition result
+        """
         self.destroy_publisher(self.event_pub)
         self.destroy_service(self.insert_query_service)
 
         self.get_logger().info(self.get_name() + ' :on_cleanup() is called.')
         return TransitionCallbackReturn.SUCCESS
 
-    def query_service_cb(self, req, response):
-        # self.get_logger().info(
-        #     '{} query requested: {}'.format(req.query_type, req.query))
+    def query_service_cb(
+         self,
+         req: ros_typedb_msgs.srv.Query.Request,
+         response: ros_typedb_msgs.srv.Query.Response
+         ) -> ros_typedb_msgs.srv.Query.Response:
+        """
+        Callback for ~/query service.
+
+        Perform the query requested with the ~/query service.
+
+        :param req: query to be performed
+        :param response: query result
+        :return: query result
+        """
         if req.query_type == 'insert':
             query_func = self.typedb_interface.insert_database
         elif req.query_type == 'delete':
