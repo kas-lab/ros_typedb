0cdbd5245d968ab7cf63ecf9d897c197b5d3a0ad
Gustavo Rezende <g.rezendesilva@tudelft.nl>
2026-04-09 11:49:13 -0300

    chore: bump typedb_utils to 0.1.1

--- typedb_utils/setup.py
+++ typedb_utils/setup.py
@@ -16,7 +16,7 @@ else:
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
